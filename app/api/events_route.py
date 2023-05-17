b80d43800bc2c8992e3b422309575a8b6e33da75
willmchristensen <willmchristensen@gmail.com>
2023-05-17 00:34:53 -0700

    edit event;

--- app/api/events_route.py
+++ app/api/events_route.py
@@ -59,13 +59,37 @@ def delete_one_event(id):
     if current_user.id == event.owner_id:
         db.session.delete(event)
         db.session.commit()
-        return "Question Deleted"
+        return "Event Deleted"
     else:
-        return {"errors": "You must be the owner of a question to delete that question."}
+        return {"errors": "You must be the owner of a event to delete that event."}
 
+@event_routes.route("/<int:id>", methods=["PUT"])
+@login_required
+def edit_one_event(id):
+    """
+    Edit event
+    """
+    form = EventForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        data = form.data
+        event = Event.query.get(id)
 
-# title
-# description
-# date
-# location
-# image_url
+        if current_user.id == event.owner_id:
+            event.title = data['title']
+            event.description = data['description']
+            event.date = data['date']
+            event.location = data['location']
+            event.image_url = data['image_url']            
+
+            db.session.commit()
+            return {
+                "event": event.to_dict()
+            }
+        else:
+            return {"errors": "You must be the owner of a event to edit that event."}
+
+    return {
+        "errors": form.errors
+    }
+
