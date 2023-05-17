93673b606e8aa922623893887aa77711d8a9fa29
willmchristensen <willmchristensen@gmail.com>
2023-05-17 00:23:17 -0700

    some of my routes were deleted lol, fml

--- app/api/events_route.py
+++ app/api/events_route.py
@@ -24,3 +24,27 @@ def get_one_event(id):
 @event_routes.route('/new', methods=["POST"])
 @login_required
 def create_one_event():
+    """
+    create event
+    """
+    form = EventForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        data = form.data
+        new_event = Event(
+            owner_id = data['owner_id'],
+            title = data['title'],
+            description = data['description'],
+            date = data['date'],
+            location = data['location'],
+            image_url = data['image_url']
+        )
+        db.session.add(new_event)
+        db.session.commit()
+        return {
+            "event": new_event.to_dict()
+        }
+
+    return {
+        "errors": form.errors
+    }
