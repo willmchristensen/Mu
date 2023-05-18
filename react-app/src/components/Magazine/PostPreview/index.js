71cef3259cd887bb7725dc5fcb159da28220c8c8
willmchristensen <willmchristensen@gmail.com>
2023-05-18 08:54:46 -0700

    magazine in the morning

--- react-app/src/components/Magazine/PostPreview/index.js
+++ react-app/src/components/Magazine/PostPreview/index.js
@@ -1,18 +1,20 @@
 import './PostPreview.css';
 
 const PostPreview = ({post, type}) => {
-
+    // conditional rendering a descriptive component for a variety of content cards.
     if(!post) return null
 
-    const time = post.createdAt.substring(11,16);
-    const sentence = post.description.split('.')[0].trim();
+    // classes----------
     const dynamicTitle = type === 'large' ? 'big-title' : 'post-title';
     const textClass = type === 'news' ?  "post-preview-text" : "news-preview-container";
     const options = { day: 'numeric', month: 'long', year: 'numeric'}
+    // data-----------
     let date = new Date(post.createdAt).toLocaleDateString('en-US', options);
     let [month, day,year] = date.split(' ');
     let formattedDate = `${day} ${month} ${year}`;
-
+    const time = post.createdAt.substring(11,16);
+    const sentence = post.description.split('.')[0].trim();
+    
     return (
 
         <div className="post-preview-container">
